1cca8031cd1d01c091c38071eb2a3eb89b2fe8e2
N-eeraj <neeraj7rajeena@gmail.com>
2024-12-22 21:27:34 +0530

    Intercepted video navigation

---
+++ src/components/video/View.tsx
@@ -0,0 +1,37 @@
+import MediaHeader from "@components/MediaHeader"
+import type { SizeOptions } from "@components/MediaHeader"
+import VideoPreview from "@components/video/Preview"
+import { fetchVideoById } from "@lib/fetchVideos"
+
+export default async function VideoView({ id }: { id: number | string }) {
+  const videoData = await fetchVideoById(id)
+
+  if (!videoData) return
+
+  const sizeOptions = videoData.video_files.reduce<SizeOptions>((options, { quality, width, height, link }) => {
+    if (quality) {
+      options.push({
+        text: quality,
+        size: `${width}x${height}`,
+        url: link,
+      })
+    }
+    return options
+  }, [])
+
+  return (
+    <section className="grid md:grid-cols-2 justify-items-center items-center gap-y-4 w-full p-2">
+      <MediaHeader
+        photographer={videoData.user.name}
+        photographerUrl={videoData.user.url}
+        sizeOptions={sizeOptions}
+        userClassName="w-full md:-order-1"
+        downloadClassName="w-full md:w-fit md:place-self-end order-2 md:order-1" />
+
+      <VideoPreview
+        url={videoData.video_files[0].link}
+        duration={videoData.duration}
+        className="md:col-span-2 order-1" />
+    </section>
+  )
+}
